3702fff459b56a2b8ab1ff5a60931288a971042a
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2026-03-06 18:10:24 +0100

    feat(lib): add mastodon and bluesky id fields to publication type

--- src/lib/publication.rs
+++ src/lib/publication.rs
@@ -9,7 +9,9 @@ pub struct Publication {
     id: Ulid,
     content: String,
     pub_date: Zoned,
+    mastodon_id: Option<String>,
     mastodon_url: Option<String>,
+    bluesky_id: Option<String>,
     bluesky_url: Option<String>,
 }
 
@@ -19,14 +21,18 @@ impl Publication {
         id: Ulid,
         content: String,
         pub_date: Zoned,
+        mastodon_id: Option<String>,
         mastodon_url: Option<String>,
+        bluesky_id: Option<String>,
         bluesky_url: Option<String>,
     ) -> Self {
         Self {
             id,
             content,
             pub_date,
+            mastodon_id,
             mastodon_url,
+            bluesky_id,
             bluesky_url,
         }
     }
@@ -41,6 +47,6 @@ impl From<NewPublicationRequest> for Publication {
     fn from(value: NewPublicationRequest) -> Self {
         let id = Ulid::new();
         let pub_date = Timestamp::now().to_zoned(TimeZone::UTC);
-        Self::new(id, value.content, pub_date, None, None)
+        Self::new(id, value.content, pub_date, None, None, None, None)
     }
 }
